6f0e7abb4b8ec323b86557f9106fcca98d497fc4
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-09-04 21:43:25 +0200

    feat: added staff controller methods and staff routes

---
+++ controllers/staff.controller.js
@@ -0,0 +1,58 @@
+import asyncWrapper from "../middlewares/async.js";
+import UserModel from "../models/user.model.js";
+
+export const getAllStaff = asyncWrapper(async (req, res, next) => {
+    const {role, departmentId} = req.query;
+
+    const filter = {};
+    if (role) {
+        filter.role = role;
+    }
+    if (departmentId) {
+        filter.departmentId = departmentId;
+    }
+
+    const staff = await UserModel.find(filter)
+        .populate('departmentId')
+        .select('-password'); // Exclude the password field
+
+    if (staff.length === 0) {
+        return res.status(404).json({
+            message: "No staff members found for the provided filters."
+        });
+    }
+
+    res.status(200).json({
+        message: "Staff members retrieved successfully",
+        staff
+    });
+
+
+    }
+)
+
+
+export const getStaffById = asyncWrapper(async (req, res, next) => {
+    const { id } = req.params;
+
+    if (!id) {
+        return res.status(400).json({
+            message: "Staff ID is required."
+        });
+    }
+
+    const staff = await UserModel.findById(id)
+        .populate('departmentId')
+        .select('-password');
+
+    if (!staff) {
+        return res.status(404).json({
+            message: "Staff member not found."
+        });
+    }
+
+    res.status(200).json({
+        message: "Staff member retrieved successfully",
+        staff
+    });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import userRouter from './user.routes.js';
-import {contactRoute} from './contact-us.routes.js';
+import contactRoute from './contact-us.routes.js';
 import departmentRoute  from './department.routes.js';
+import staffRoutes from "./staff.routes.js";
 
 const routes = express.Router();
 
 routes.use('/auth', userRouter);
 routes.use("/contact-us",contactRoute);
 routes.use('/departments', departmentRoute);
+routes.use('/staff', staffRoutes);
 
 export default routes;
