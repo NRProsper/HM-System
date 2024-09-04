d33cee87ee3982d880c543072953df7afd46028d
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-09-04 20:42:44 +0200

    fix: userRouter import fixed

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import {userRouter} from './user.routes.js';
+import userRouter from './user.routes.js';
 import {contactRoute} from './contact-us.routes.js';
 import departmentRoute  from './department.routes.js';
 
 const routes = express.Router();
 
-routes.use('/user', userRouter);
+routes.use('/auth', userRouter);
 routes.use("/contact-us",contactRoute);
 routes.use('/departments', departmentRoute);
 
