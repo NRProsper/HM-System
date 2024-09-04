39f19fdf4690e49fe3362d9aa234b52cf7501ce3
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-09-04 12:51:17 +0200

    refactors: added a root route and commented ErrorHandlerMiddleware

--- services/ExpressApp.js
+++ services/ExpressApp.js
@@ -1,5 +1,5 @@
 import express from "express";
-import  { routes } from "../routes/index.js";
+import routes  from "../routes/index.js";
 import cors from "cors";
 import cookieParser from "cookie-parser"
 import documentation from '../docs/swagger.json' assert {"type":"json"}
@@ -16,11 +16,15 @@ export default function (app) {
     app.use(cors());
     app.use(express.json());
 
+    app.get("/", (req, res) => {
+        res.send("Welcome to health management API v.1.0.0");
+    })
+
     app.use('/api', routes);
     app.use(cookieParser());
     app.use('/api-documentation', swaggerUi.serve, swaggerUi.setup(documentation))
 
-    app.use(ErrorHandlerMiddleware);
+    // app.use(ErrorHandlerMiddleware);
 
    
     return app;
