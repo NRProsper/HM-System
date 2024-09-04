67b532f7b4a70da37cf6af283cd3387a2e289ca7
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-09-04 20:29:10 +0200

    refactor: enchanced the createUser and login methods

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,38 +5,51 @@ import asyncWrapper from "../middlewares/async.js";
 import { BadRequestError } from "../errors/BadRequestError.js";
 import { validationResult } from "express-validator";
 import crypto from "crypto";
+import UserModel from "../models/user.model.js";
 
-export const signUp = asyncWrapper(async (req, res, next) => {
+export const createUser = asyncWrapper(async (req, res, next) => {
     // Validate request
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return next(new BadRequestError(errors.array()[0].msg));
     }
+
+    const { firstName, lastName, email, phone, description, password, role, departmentId } = req.body;
   
     // Check if the email already exists
-    const existingUser = await userModel.findOne({ email: req.body.email });
+    const existingUser = await userModel.findOne({ $or: [{ email }, { phone }] });
     if (existingUser) {
-      return next(new BadRequestError("Email already in use!"));
+        return next(new BadRequestError("Email or phone number already in use!"));
     }
   
     // Create new user
-    const newUser = new userModel({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-      role: req.body.role,
+    const newUser = new UserModel({
+        firstName,
+        lastName,
+        email,
+        phone,
+        description,
+        password,
+        role,
+        departmentId,
     });
   
     await newUser.save();
-  
+
     res.status(201).json({
-      message: "User registered successfully!",
-      user: {
-        id: newUser.id,
-        email: newUser.email,
-        role: newUser.role,
-      },
+        message: "User registered successfully!",
+        user: {
+            id: newUser.id,
+            email: newUser.email,
+            role: newUser.role,
+            firstName: newUser.firstName,
+            lastName: newUser.lastName,
+            phone: newUser.phone,
+            description: newUser.description,
+            departmentId: newUser.departmentId
+        },
     });
+
   });
 
   export const logIn = asyncWrapper(async (req, res, next) => {
@@ -45,35 +58,51 @@ export const signUp = asyncWrapper(async (req, res, next) => {
     if (!errors.isEmpty()) {
       return next(new BadRequestError(errors.array()[0].msg));
     }
+
+    const { email, password } = req.body;
   
     // Find user
-    const foundUser = await userModel.findOne({ email: req.body.email });
-    
-  
-    // Check account verification
-    
+      const foundUser = await UserModel.findOne({ email }).populate('departmentId');
+      if (!foundUser) {
+          return next(new BadRequestError("Invalid email or password!"));
+      }
   
     // Verify password
-    const isPasswordVerfied = await bcryptjs.compareSync(
-      req.body.password,
-      foundUser.password
-    );
-    if (!isPasswordVerfied) {
-      return next(new BadRequestError("Invalid email or password!"));
-    }
+      const isPasswordVerified = await bcryptjs.compare(password, foundUser.password);
+      if (!isPasswordVerified) {
+          return next(new BadRequestError("Invalid email or password!"));
+      }
   
     // Generate token
-    const token = jwt.sign(
-      { id: foundUser.id, email: foundUser.email },
-      process.env.JWT_SECRET_KEY,
-      { expiresIn: "1h" }
-    );
-    
-    res.status(200).json({
-      message: "User logged in!",
-      token: token,
-      user: foundUser,
-    });
+      const token = jwt.sign(
+          {
+              id: foundUser.id,
+              email: foundUser.email,
+              role: foundUser.role,
+              firstName: foundUser.firstName,
+              lastName: foundUser.lastName,
+              phone: foundUser.phone,
+              departmentId: foundUser.departmentId ? foundUser.departmentId._id : null,
+              departmentName: foundUser.departmentId ? foundUser.departmentId.name : null
+          },
+          process.env.JWT_SECRET_KEY,
+          { expiresIn: "1h" }
+      );
+
+      res.status(200).json({
+          message: "User logged in!",
+          token: token,
+          user: {
+              id: foundUser.id,
+              email: foundUser.email,
+              role: foundUser.role,
+              firstName: foundUser.firstName,
+              lastName: foundUser.lastName,
+              phone: foundUser.phone,
+              departmentId: foundUser.departmentId ? foundUser.departmentId._id : null,
+              departmentName: foundUser.departmentId ? foundUser.departmentId.name : null
+          }
+      });
   });
   
   
