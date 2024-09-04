25f2250261232ecde97ccc752e7502e09e1a17d1
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-09-04 12:53:06 +0200

    refactor: modified the department route in inde.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import {userRouter} from './user.routes.js';
 import {contactRoute} from './contact-us.routes.js';
-import { departmentRoute } from './department.routes.js';
+import departmentRoute  from './department.routes.js';
 
-export const routes = express.Router();
+const routes = express.Router();
 
 routes.use('/user', userRouter);
 routes.use("/contact-us",contactRoute);
-routes.use('/department',departmentRoute);
+routes.use('/departments', departmentRoute);
+
+export default routes;
