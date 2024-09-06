12aed851f38eac04d7397236ea593d53d732eacd
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-09-06 02:45:23 +0200

    feat: Added appointments controller, routes

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import userRouter from './user.routes.js';
 import contactRoute from './contact-us.routes.js';
 import departmentRoute  from './department.routes.js';
 import staffRoutes from "./staff.routes.js";
+import appointmentRoutes from "./appointment.routes.js";
 
 const routes = express.Router();
 
@@ -10,5 +11,6 @@ routes.use('/auth', userRouter);
 routes.use("/contact-us",contactRoute);
 routes.use('/departments', departmentRoute);
 routes.use('/staff', staffRoutes);
+routes.use('/appointments', appointmentRoutes);
 
 export default routes;
