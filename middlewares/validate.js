df81031fc36111d4552439958091bce0f6920807
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-09-08 18:26:26 +0200

    fix(validation): fixed validation reducer method to arrange errors

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -3,7 +3,23 @@ import { validationResult } from 'express-validator';
 export const validateRequest = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        const groupedErrors = errors.array().reduce((acc, err) => {
+            if (!acc[err.path]) {
+                acc[err.path] = [];
+            }
+            acc[err.path].push({
+                msg: err.msg,
+                type: err.type,
+                location: err.location
+            });
+            return acc;
+        }, {});
+
+        return res.status(400).json({
+            status: "error",
+            message: "Validation failed",
+            errors: groupedErrors
+        });
     }
     next();
 };
