e0d8f9cf0f79709a1cb8e66c3ebbf2c3125aa8c0
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-09-09 15:56:10 +0200

    feat(appointments): All aperations work

--- routes/appointment.routes.js
+++ routes/appointment.routes.js
@@ -1,9 +1,17 @@
 import express from "express";
-import {createAppointment, getAllAppointments} from "../controllers/appointment.controller.js";
+import {
+    approveAppointment,
+    createAppointment,
+    getAllAppointments,
+    rejectAppointment
+} from "../controllers/appointment.controller.js";
+import {authorizedRoles, verifyToken} from "../middlewares/authorization.js";
 
 const appointmentRoutes = express.Router();
 
 appointmentRoutes.post("/", createAppointment);
-appointmentRoutes.get("/", getAllAppointments);
+appointmentRoutes.get("/",verifyToken, authorizedRoles("Admin", "Doctor"), getAllAppointments);
+appointmentRoutes.post('/:appointmentId/approve',verifyToken, authorizedRoles("Doctor", "Admin"), approveAppointment);
+appointmentRoutes.post('/:appointmentId/reject',verifyToken, authorizedRoles("Doctor", "Admin"), rejectAppointment);
 
 export default appointmentRoutes;
