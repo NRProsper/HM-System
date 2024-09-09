7896a24279f729fe58935637033612fa09fb425d
NISHIMWE RUKUNDO Prosper <prosper.rk1@gmail.com>
2024-09-09 15:52:44 +0200

    feat(mail): Utity to be used to send Emails to clients

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,26 +1,33 @@
-import nodemailer from "nodemailer"
-export const sendEmail=(receipt,subject,body)=>{
- const transporter=nodemailer.createTransport({
-    service:process.env.EMAIL_SERVICE,
-    auth:{
-        user:process.env.EMAIL_USER,
-        pass:process.env.EMAIL_PASSWORD
+import nodemailer from "nodemailer";
+
+
+const transporter = nodemailer.createTransport({
+    service: process.env.EMAIL_SERVICE,
+    host: process.env.EMAIL_HOST,
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD
     },
     tls: {
         rejectUnauthorized: false,
     }
- })
- const mailOptions = {
-    from: '"Benigne" <benigneniyonizera58@gmail.com>',
-    to: receipt,
-    subject: subject,
-    text: body
+});
+
+export const sendEmail = (receipt, subject, body) => {
+    const mailOptions = {
+        from: `"CareSync" <${process.env.EMAIL_USER}>`,
+        to: receipt,
+        subject: subject,
+        html: body,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error sending email:', error);
+        } else {
+            console.log('Email sent:', info.response);
+        }
+    });
 };
-transporter.sendMail(mailOptions,(error,info)=>{
-    if (error) {
-        console.error('Error sending email:', error);
-    } else {
-        console.log('Email sent:', info.response);
-    }
-})
-}
